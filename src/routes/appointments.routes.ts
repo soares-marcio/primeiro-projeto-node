aba86ebc62db85b939873de8acf5d0e5f0f32588
Marcio <biodevs@icloud.com>
2020-06-27 17:06:49 -0300

    first commit

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,31 @@
+import { Router, request, response } from 'express';
+import { startOfHour, parseISO } from 'date-fns';
+import AppointmentsRepository from '../repositories/AppoitmentsRepository';
+
+const appointmentsRouter = Router();
+
+const appointmentsRepository = new AppointmentsRepository();
+
+appointmentsRouter.get('/', (request, response) => {
+  const appointments = appointmentsRepository.all();
+  return response.json(appointments);
+});
+
+appointmentsRouter.post('/', (request, response) => {
+  const { provider, date } = request.body;
+  const parsedDate = startOfHour(parseISO(date));
+
+  const findAppointmentIsSameDate = appointmentsRepository.findByDate(parsedDate);
+
+  if (findAppointmentIsSameDate) {
+    return response.status(400).json({ message: 'This appointment is already booked.'})
+  }
+
+  const appointment = appointmentsRepository.create({
+    provider,
+    date: parsedDate
+  });
+  return response.json(appointment)
+});
+
+export default appointmentsRouter;
